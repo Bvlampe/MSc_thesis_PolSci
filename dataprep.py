7ba4764b6ee7c88fb0af0ac0c6fd2eef3d70617e
Bjarne <bjarne@vonlampe.de>
2023-03-25 15:22:06 +0100

    minor

--- dataprep.py
+++ dataprep.py
@@ -1,4 +1,14 @@
+import pandas as pd
+
+
 def dataprep():
-    path_glob = "globalisation.csv"
-    path_iusers = "internet_users.csv"
-    path_lit = "literacy_rate.csv"
+    path_rawdata = "datasets_input/"
+
+    path_glob = path_rawdata + "globalisation.csv"
+    path_iusers = path_rawdata + "internet_users.csv"
+    path_lit = path_rawdata + "literacy_rate.csv"
+
+    raw_glob = pd.read_csv(path_glob, encoding='cp1252')
+    raw_iusers = pd.read_csv(path_iusers)
+    raw_lit = pd.read_csv(path_lit)
+    print(raw_glob.head())
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 import dataprep
+
+dataprep.dataprep()
