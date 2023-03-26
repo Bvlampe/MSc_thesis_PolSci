4cbba865261efbba631d2f53152ec2cf52e70a76
Bjarne <bjarne@vonlampe.de>
2023-03-26 23:44:36 +0200

    Added GTD incl. cutting unnecessary data and reshaping to country-year format

--- dataprep.py
+++ dataprep.py
@@ -1,14 +1,36 @@
 import pandas as pd
 
 
+# Cuts unnecessary data from the 100MB GTD and exports the 10x smaller dataframe back to a .csv file
+def cut_GTD(path_in, path_out, code="cp1252"):
+    in_data = pd.read_csv(path_in, encoding=code)
+    in_data = in_data[["eventid", "iyear", "country", "country_txt", "success", "INT_LOG", "INT_IDEO"]]
+    in_data.to_csv(path_out)
+    return in_data
+
+
+# Transforms the GTD into a country-year format
+def format_GTD(in_data):
+    out_data = pd.DataFrame(columns=range(in_data["iyear"].min(), in_data["iyear"].max() + 1))
+    for _, row in in_data.iterrows():
+        if True: # Add possible inclusion criteria here
+            out_data.loc[row["country_txt"], row["iyear"]] = True
+    out_data.fillna(value=False, inplace=True)
+    return out_data
+
+
 def dataprep():
     path_rawdata = "datasets_input/"
 
+    path_GTD_raw = path_rawdata + "GTD_raw.csv"
+    path_GTD = path_rawdata + "GTD.csv"
     path_glob = path_rawdata + "globalisation.csv"
     path_iusers = path_rawdata + "internet_users.csv"
     path_lit = path_rawdata + "literacy_rate.csv"
 
-    raw_glob = pd.read_csv(path_glob, encoding='cp1252')
+    # raw_GTD = cut_GTD(path_GTD_raw, path_GTD)
+    raw_GTD = pd.read_csv(path_GTD, encoding="cp1252")
+    raw_glob = pd.read_csv(path_glob, encoding="cp1252")
     raw_iusers = pd.read_csv(path_iusers)
     raw_lit = pd.read_csv(path_lit)
-    print(raw_glob.head())
+    print(format_GTD(raw_GTD).head())
