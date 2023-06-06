10262f492d19389634720cafb6799526f045af71
Bjarne <bjarne@vonlampe.de>
2023-06-06 22:14:02 +0200

    Updated dataexpl.py to investigate where to split between train and test sets

--- dataexpl.py
+++ dataexpl.py
@@ -23,3 +23,12 @@ def dataexpl():
     attacks.sort_values(by="Population", inplace=True)
     attacks.plot(x="Population", y="Attacks", kind="scatter", logx=True, title="Terrorist attack frequency over population")
     plt.show()
+
+    main_data['Year'] = main_data.index.get_level_values(1)
+    for col in main_data.columns:
+        main_data[col] = main_data.groupby(level=0)[col].apply(
+            lambda group: group.interpolate(limit=10, limit_area="inside"))
+    main_data.dropna(inplace=True)
+    for year in main_data.index.get_level_values(1).unique().sort_values():
+        uptoincl = len(main_data.loc[main_data['Year'].isin(range(year + 1))])
+        print(f"Percentage of cases up to and including year {year}: {100 * uptoincl / len(main_data)}%")
