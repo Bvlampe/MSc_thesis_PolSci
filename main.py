7cba2cfcca313d923dcd0a2f93729ac3b84766e7
Bjarne <bjarne@vonlampe.de>
2023-06-10 12:59:22 +0200

    Outdated code and log files removed

--- main.py
+++ main.py
@@ -5,18 +5,6 @@ import models
 import resexpl
 
 
-# Old function calls, not in use anymore
-def old():
-    for extra in [[], ["interpol_glob_GTD"], ["education"], ["interpol_glob_GTD", "education"]]:
-        auc_roc_set = pd.DataFrame()
-        for vars in ["academic", "professional", "combined", "all", "notrade"]:
-            models.partial_models(varchoice=vars, macrolog=auc_roc_set, extra_options=extra)
-        suffix = '_' + "+".join(extra) if extra else ''
-        auc_roc_set.to_csv("output_files/auc_roc_per_model" + suffix + ".csv")
-    auc_roc_set = pd.DataFrame()
-    models.partial_models(varchoice="nogdp", macrolog=auc_roc_set, extra_options=[])
-    auc_roc_set.to_csv("output_files/auc_roc_per_model_nogdp.csv")
-
 def main():
     # dataprep.dataprep("edit", edit_col="Global terrorism")
     # dataexpl.dataexpl()
